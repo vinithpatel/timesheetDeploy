5f6f13d87f12da7e5ff3088fe444913aa5b84e96
Vinith <viniththammala12@gmail.com>
2023-12-23 20:26:52 +0530

    package.json update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/timesheetDeploy/'  // Change this to your repository name
+    : '/',
+
+
   transpileDependencies: true,
 
   pluginOptions: {
