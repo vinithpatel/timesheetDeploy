e3bff435b8cc679c58c0ac2e1b5725ef61a1ae11
Vinith <viniththammala12@gmail.com>
2023-12-27 15:30:35 +0530

    nav bar active link fixed

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -8,6 +8,8 @@
             prepend-avatar="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
             :title="$store.state.employeeName"
             nav
+            :active="isLinkActive('profile')"
+            color="primary"
           >
           </v-list-item>
         </router-link>
@@ -18,37 +20,53 @@
           <div class="mb-5">
             <v-list-subheader class="text-subtitle-1">EMPLOYEE</v-list-subheader>
             <router-link to="/submit_timesheet/" class="router-link">     
-              <v-list-item prepend-icon="mdi-file-send" title="Submit Timesheet" value="home"></v-list-item>
+              <v-list-item :active="isLinkActive('submit_timesheet')" prepend-icon="mdi-file-send" title="Submit Timesheet" value="home"
+              color="#6a70eb"
+              ></v-list-item>
             </router-link> 
             <router-link to="/" class="router-link">
-              <v-list-item prepend-icon="mdi-text-box-search" title="Search Timesheet" value="searchTimesheet"></v-list-item>
+              <v-list-item :active="isLinkActive('')" prepend-icon="mdi-text-box-search" title="Search Timesheet" value="searchTimesheet"
+              color="#6a70eb"
+              ></v-list-item>
             </router-link>
 
             <router-link to="/review_timesheets" class="router-link">
-              <v-list-item prepend-icon="mdi-file-check" title="Review Timesheets" value="reviewTimesheets"></v-list-item>
+              <v-list-item :active="isLinkActive('review_timesheets')" prepend-icon="mdi-file-check" title="Review Timesheets" value="reviewTimesheets"
+              color="#6a70eb"
+              ></v-list-item>
             </router-link>
 
           </div>
           <div v-if="isAdmin">
               <v-list-subheader class="text-subtitle-1">Admin</v-list-subheader>      
               <router-link to="/" class="router-link">
-                <v-list-item prepend-icon="mdi-account-search" title="Employee Timesheet" value="employeeTimesheet"></v-list-item>
+                <v-list-item :active="isLinkActive('')" prepend-icon="mdi-account-search" title="Employee Timesheet" value="employeeTimesheet"
+                color="#6a70eb"
+                ></v-list-item>
               </router-link>
 
               <router-link to="/employees" class="router-link">
-                <v-list-item prepend-icon="mdi-account-supervisor" title="Employees" value="employees"></v-list-item>
+                <v-list-item :active="isLinkActive('employees')" prepend-icon="mdi-account-supervisor" title="Employees" value="employees"
+                color="#6a70eb"
+                ></v-list-item>
               </router-link>
 
               <router-link to="/customers" class="router-link">
-                <v-list-item prepend-icon="mdi-domain" title="Customers" value="customers"></v-list-item>
+                <v-list-item :active="isLinkActive('customers')" prepend-icon="mdi-domain" title="Customers" value="customers"
+                color="#6a70eb"
+                ></v-list-item>
               </router-link>
 
               <router-link to="/projects" class="router-link">
-                  <v-list-item prepend-icon="mdi-file-multiple" title="Projects" value="projects"></v-list-item>
+                  <v-list-item :active="isLinkActive('projects')" prepend-icon="mdi-file-multiple" title="Projects" value="projects"
+                  color="#6a70eb"
+                  ></v-list-item>
               </router-link>
 
               <router-link to="/export" class="router-link">
-                <v-list-item prepend-icon="mdi-export" title="Export Timesheet" value="export"></v-list-item>
+                <v-list-item :active="isLinkActive('export')" prepend-icon="mdi-export" title="Export Timesheet" value="export"
+                color="#6a70eb"
+                ></v-list-item>
               </router-link>
             </div>
         </v-list>
@@ -72,6 +90,12 @@
         computed:{
         ...mapState(['isAdmin'])
         },
+
+        methods:{
+          isLinkActive(path){
+            return path === this.$route.path.split('/')[1]
+          }
+        }
 
     })
 </script>
